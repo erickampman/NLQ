d7257770c17c384ad5d045ad04db198bef14000c
Eric Kampman <erickampman@yahoo.com>
2021-05-25 10:11:27 -0700

    Initial Commit

---
+++ NLQ/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NLQ
+//
+//  Created by Eric Kampman on 5/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
